d0de7085a8493a64581b312717635561c3c5bb22
zainarbani <zaintsyariev@gmail.com>
2023-09-06 17:03:02 +0700

    drivers/misc/mediatek/power: Import OEM changes
    Change-Id: Ie01fe993e2331a6f3cdce7e80b6af1e9dc35169d
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- drivers/misc/mediatek/power/mt6785/mtk_battery_intf.c
+++ drivers/misc/mediatek/power/mt6785/mtk_battery_intf.c
@@ -128,11 +128,15 @@ signed int battery_get_uisoc(void)
 
 signed int battery_get_bat_temperature(void)
 {
+#if 1
 	/* TODO */
 	if (is_battery_init_done())
 		return force_get_tbat(true);
 	else
 		return -127;
+#else
+		return 25;
+#endif
 }
 
 signed int battery_get_ibus(void)
