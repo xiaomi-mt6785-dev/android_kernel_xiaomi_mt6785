61775834cea2751c9124102700710f69ee5ce0a6
probaero <87478728+probaero@users.noreply.github.com>
2026-07-02 10:59:55 +0700

    video: ddp: rdma: fix ioctl function pointer signature mismatch
    rdma_ioctl indirectly calls a module-specific ioctl handler with mismatching pointer types, causing CFI check to fail
    
    The rdma_ioctl function signature used an incomplete enum type, which caused a signature mismatch when assigning the callback to the dispatch structure's .ioctl member. Match the pointer types by using the correct global enum DDP_IOCTL_NAME to pass CFI check.
    
    Change-Id: I690056ba5fb61639b3f838d3ffd06d189547d583

--- drivers/misc/mediatek/video/mt6785/dispsys/ddp_rdma_ex.c
+++ drivers/misc/mediatek/video/mt6785/dispsys/ddp_rdma_ex.c
@@ -1219,13 +1219,12 @@ static int _rdma_partial_update(enum DISP_MODULE_ENUM module, void *arg,
 }
 
 int rdma_ioctl(enum DISP_MODULE_ENUM module, void *cmdq_handle,
-	       unsigned int ioctl_cmd, unsigned long *params)
+	       enum DDP_IOCTL_NAME ioctl_cmd, void *params)
 {
 	int ret = 0;
-	enum DDP_IOCTL_NAME ioctl = (enum DDP_IOCTL_NAME)ioctl_cmd;
 	unsigned int idx = rdma_index(module);
 
-	switch (ioctl) {
+	switch (ioctl_cmd) {
 	case DDP_RDMA_GOLDEN_SETTING:
 	{
 		struct disp_ddp_path_config *pConfig;
@@ -1303,7 +1302,6 @@ struct DDP_MODULE_DRIVER ddp_driver_rdma = {
 	.build_cmdq = rdma_build_cmdq,
 	.set_lcm_utils = NULL,
 	.enable_irq = rdma_enable_irq,
-	.ioctl = (int (*)(enum DISP_MODULE_ENUM, void *,
-			  enum DDP_IOCTL_NAME, void *))rdma_ioctl,
+	.ioctl = rdma_ioctl,
 	.switch_to_nonsec = NULL,
 };
