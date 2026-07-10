f29df7768992f3fa7198345fadd45f34882bf46f
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-10 13:06:17 +0200

    drivers: input: Guard update_userlimit_cpu_freq usage
    Change-Id: I5123938aa6d4b23e396438cf007a5713cd2fba94

--- drivers/input/fingerprint_C3J/goodix/gf_spi_tee.c
+++ drivers/input/fingerprint_C3J/goodix/gf_spi_tee.c
@@ -1145,8 +1145,10 @@ static void freq_release(struct work_struct *work)
 	}
 	if (atomic_read(&boosted) == 1) {
 		gf_debug(INFO_LOG, "%s  release freq lock\n", __func__);
+#ifdef CONFIG_MTK_CPU_CTRL
 		update_userlimit_cpu_freq(CPU_KIR_FP, cluster_num,
 					freq_to_set);
+#endif
 		atomic_dec(&boosted);
 	}
 }
@@ -1173,8 +1175,10 @@ static int freq_hold(int sec)
 	if (atomic_read(&boosted) == 0) {
 		printk("%s: fingerprint down\n", __func__);
 		gf_debug(INFO_LOG, "%s for %d * 500 msec \n", __func__, sec);
+#ifdef CONFIG_MTK_CPU_CTRL
 		update_userlimit_cpu_freq(CPU_KIR_FP, cluster_num,
 					freq_to_set);
+#endif
 		atomic_inc(&boosted);
 		release_timer.expires = jiffies + (HZ / 2) * sec;
 		add_timer(&release_timer);
